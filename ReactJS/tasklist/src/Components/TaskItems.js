39c0405413103fbdf5cecddc5c4cb71d306915ff
Sarabdeep93 <sarabdeepsk@gmail.com>
2021-03-08 13:23:29 +0530

    Update TaskItems.js
    Updated TaskItems

--- ReactJS/tasklist/src/Components/TaskItems.js
+++ ReactJS/tasklist/src/Components/TaskItems.js
@@ -4,12 +4,12 @@ import {RiCloseCircleLine} from 'react-icons/ri';
 import {TiEdit} from 'react-icons/ti';
 import {FaCheckCircle} from 'react-icons/fa';
 
+
 class TaskItems extends Component{ 
     render(){
         return(
             <ul className="theList taskForm">
                  <FlipMove duration={250} easing="ease-in-out">
-
                     {
                     this.props.entries.map(item =>(
                         <li key={item.key}>
@@ -17,17 +17,17 @@ class TaskItems extends Component{
                                 <span className={item.isCompleted ? "itemContent completed" : "itemContent"}>{item.text}</span>
                             </div>
                             <div className="dv-task-icons">
-                                <span className="close icons" onClick={() => this.props.deleteData(item.key)}>
+                                <a className="close icons" onClick={() => this.props.deleteData(item.key)}>
                                     <RiCloseCircleLine className="icon-delete" title="Click to delete the task" />
-                                </span>
+                                </a>
 
-                                <span className="close icons" disabled={true} onClick={() => this.props.editTask(item.key, item.text)}>
-                                    <TiEdit disabled={true} className="icon-edit" title="Click to edit the task" />
-                                </span>
+                                <a id="btnEdit" className="close icons" disabled={item.isCompleted ? true : false}  onClick={() => this.props.editTask(item.key, item.text)}>
+                                    <TiEdit className="icon-edit" title="Click to edit the task" />
+                                </a>
 
-                                <span className="close icons" disabled={item.isCompleted ? true : false} onClick={() => this.props.markComplete(item.key)}>
+                                <a className="close icons" disabled={item.isCompleted ? true : false} onClick={() => this.props.markComplete(item.key)}>
                                     <FaCheckCircle disabled={item.isCompleted ? true : false} className="icon-complete" title="Click to mark task as completed" />
-                                </span>
+                                </a>
                             </div>
                         </li>
                     ))}
